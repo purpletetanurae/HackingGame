58ef065465cd131c35449c971ef10daad04c2771
purpletetanurae <48185527+purpletetanurae@users.noreply.github.com>
2019-10-15 14:31:02 +0300

    Добавил fps и врщающегося моба

--- HackingGame/Mob/Mob.cs
+++ HackingGame/Mob/Mob.cs
@@ -8,6 +8,11 @@ public class Mob : Node2D
 	public float _delta;
 	
 	public TaskList _taskList;
+
+	private Sprite sprite;
+	private Vector2 spriteSpeed; 
+	private double time = 0;
+	private Label label;
 	public TaskList taskList
 	{
 		set
@@ -34,11 +39,24 @@ public class Mob : Node2D
 		taskList = new TaskList();
 		taskList.Add(new WaitTask(5));
 		taskList.Add(new MoveTask(500,500));
+
+		sprite = GetNode<Sprite>("test");
+		label = GetNode<Label>("Label");
 		
 	}
 	public override void _PhysicsProcess(float delta)
 	{
 		_delta = delta;
 		taskList.Act();
+
+		time += 0.1;
+
+		spriteSpeed.x = (float)Math.Cos(time);
+		spriteSpeed.y = (float)Math.Sin(time);
+		spriteSpeed *= 100;
+
+		sprite.Position += spriteSpeed * delta;
+		label.SetText(Engine.GetFramesPerSecond().ToString());
+		
 	}
 }
